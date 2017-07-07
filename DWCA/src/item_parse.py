9092dd7e2e6da374e34ab8168c598ee50188e571
aran <Andreas.Ran@infor.com>
2017-07-07 21:56:46 +0200

    Sort of works.

--- DWCA/src/item_parse.py
+++ DWCA/src/item_parse.py
@@ -1,9 +1,12 @@
+from collections import namedtuple
 import re
 
+from src.entities import NAME, DICE, DAMAGE, QUALITIES
 from src.util.read_file import pretty_print
 
 
-CLASSES = ['Melee', 'Mounted']
+WEAPON_CLASSES = ['Melee', 'Mounted']
+RENOWN_RANKS = ['Famed', 'Distinguished', 'Respected']
 
 
 def hi():
@@ -15,25 +18,68 @@ def hi():
         for line in content:
             if 'd10' in line:
                 clean_line = re.sub(r'([^\s\w\,()+]|_)+', '', line)
+                clean_line = remove_renown_ranks(clean_line)
                 weapon_lines.append(clean_line)
-        pretty_print(weapon_lines)
-
+        weapon_definitions = {}
         for weapon_line in weapon_lines:
-            name, weapon_class = find_name(weapon_line)
-            stuff = re.findall(
-                r'(\d)d10\+*(\d*) ([IREX]) (\d*) (\w.*)?\s\d+\s\d+', weapon_line)[0]
-            dice = stuff[0]
-            flat_damage = stuff[1]
-            damage_type = stuff[2]
-            penetration = stuff[3]
-            qualities = stuff[4]
-            print name
-            print weapon_class
-            print '{}d10+{} {} Pen: {} Qual: {}'.format(dice, flat_damage, damage_type, penetration, qualities)
+            name, weapon_class = extract_name_and_class(weapon_line)
+            damage_profile = extract_damage_profile(weapon_line)
+            qualities = convert_qualities_list_to_dict(
+                damage_profile.qualities)
+            weapon_def = {
+                NAME: name,
+                'class': weapon_class,
+                DICE: damage_profile.dice,
+                DAMAGE: damage_profile.damage,
+                'damage_type': damage_profile.damage_type,
+                'penetration': damage_profile.penetration,
+                QUALITIES: qualities
+            }
+            internal_name = name.lower().strip().replace(' ', '_')
+
+            weapon_definitions[internal_name] = weapon_def
+        pretty_print(weapon_definitions)
+        return weapon_definitions
 
 
-def find_name(weapon_line):
-    for weapon_class in CLASSES:
+def extract_damage_profile(weapon_line):
+    DamageProfile = namedtuple(
+        'DmgProfile', ['dice', 'damage', 'penetration', 'damage_type', 'qualities'])
+    matches = re.findall(
+        r'(\d)d10\+*(\d*) ([IREX]) (\d*) ?(\w.*)?(\d|$)', weapon_line)[0]
+    damage_profile = DamageProfile(dice=int(matches[0]),
+                                   damage=int(
+                                       matches[1] if matches[1] != '' else 0),
+                                   damage_type=matches[2],
+                                   penetration=int(matches[
+                                       3] if matches[3] != '' else 0),
+                                   qualities=matches[4].split(','))
+    return damage_profile
+
+
+def remove_renown_ranks(weapon_line):
+    for renown_string in RENOWN_RANKS:
+        weapon_line = weapon_line.replace(renown_string, '')
+    return weapon_line
+
+
+def convert_qualities_list_to_dict(qualities_list):
+    qualities_dict = {}
+    for quality in qualities_list:
+        if '(' not in quality:
+            quality_value = True
+        else:
+            quality_value = int(re.findall(r'(\d)', quality)[0])
+        quality_name = re.sub(
+            r'([^\sa-zA-Z]|_)+', '', quality).strip().lower()
+        if quality_name != '':
+            qualities_dict[quality_name] = quality_value
+    return qualities_dict
+
+
+def extract_name_and_class(weapon_line):
+    for weapon_class in WEAPON_CLASSES:
         split_list = weapon_line.split(weapon_class)
         if len(split_list) > 1:
-            return split_list[0], weapon_class
+            name = split_list[0].strip()
+            return name, weapon_class
---
+++ DWCA/test/tests_item_parse.py
@@ -0,0 +1,308 @@
+'''
+Created on 7 Jul 2017
+
+@author: Dos'
+'''
+import unittest
+from src.item_parse import hi
+
+
+class Test(unittest.TestCase):
+
+    def testName(self):
+        expected = self.cool_dict
+        actual = hi()
+
+        self.assertEqual(expected, actual)
+
+    def setUp(self):
+        self.maxDiff = None
+        self.cool_dict = {
+            "omnissian_axe_(astartespattern)": {
+                "dice": 1,
+                "flat_damage": 11,
+                "penetration": 6,
+                "damage_type": "E",
+                "qualities": {
+                    "power field": True,
+                    "unbalanced": True
+                },
+                "class": "Melee",
+                "name": "Omnissian Axe (AstartesPattern)"
+            },
+            "astartes_chainsword": {
+                "dice": 1,
+                "flat_damage": 3,
+                "penetration": 3,
+                "damage_type": "R",
+                "qualities": {
+                    "balanced": True,
+                    "tearing": True
+                },
+                "class": "Melee",
+                "name": "Astartes Chainsword"
+            },
+            "breaching_augur": {
+                "dice": 2,
+                "flat_damage": 11,
+                "penetration": 7,
+                "damage_type": "R",
+                "qualities": {
+                    "tearing": True,
+                    "power field": True,
+                    "unwieldy": True
+                },
+                "class": "Melee",
+                "name": "Breaching Augur"
+            },
+            "lash_whip": {
+                "dice": 1,
+                "flat_damage": 3,
+                "penetration": 3,
+                "damage_type": "R",
+                "qualities": {
+                    "flexible": True,
+                    "snare": True
+                },
+                "class": "Melee",
+                "name": "Lash Whip"
+            },
+            "astartes_power_axe": {
+                "dice": 1,
+                "flat_damage": 8,
+                "penetration": 7,
+                "damage_type": "E",
+                "qualities": {
+                    "power field": True,
+                    "unbalanced": True
+                },
+                "class": "Melee",
+                "name": "Astartes Power Axe"
+            },
+            "astartes_power_sword": {
+                "dice": 1,
+                "flat_damage": 6,
+                "penetration": 6,
+                "damage_type": "E",
+                "qualities": {
+                    "balanced": True,
+                    "power field": True
+                },
+                "class": "Melee",
+                "name": "Astartes Power Sword"
+            },
+            "scything_talons": {
+                "dice": 1,
+                "flat_damage": 2,
+                "penetration": 3,
+                "damage_type": "R",
+                "qualities": {},
+                "class": "Melee",
+                "name": "Scything Talons"
+            },
+            "astartes_lightning_claw": {
+                "dice": 1,
+                "flat_damage": 6,
+                "penetration": 8,
+                "damage_type": "E",
+                "qualities": {
+                    "power field": True,
+                    "proven": 4
+                },
+                "class": "Melee",
+                "name": "Astartes Lightning Claw"
+            },
+            "ceremonial_sword": {
+                "dice": 1,
+                "flat_damage": 3,
+                "penetration": 2,
+                "damage_type": "R",
+                "qualities": {
+                    "balanced": True
+                },
+                "class": "Melee",
+                "name": "Ceremonial Sword"
+            },
+            "bonesword": {
+                "dice": 1,
+                "flat_damage": 0,
+                "penetration": 6,
+                "damage_type": "R",
+                "qualities": {
+                    "drain life": True
+                },
+                "class": "Melee",
+                "name": "Bonesword"
+            },
+            "astartes_power_spear": {
+                "dice": 1,
+                "flat_damage": 6,
+                "penetration": 7,
+                "damage_type": "E",
+                "qualities": {
+                    "power field": True
+                },
+                "class": "Melee",
+                "name": "Astartes Power Spear"
+            },
+            "astartes_executioner_axe": {
+                "dice": 1,
+                "flat_damage": 13,
+                "penetration": 8,
+                "damage_type": "E",
+                "qualities": {
+                    "power field": True,
+                    "felling": 1,
+                    "unwieldy": True
+                },
+                "class": "Melee",
+                "name": "Astartes Executioner Axe"
+            },
+            "crozius_arcanum": {
+                "dice": 1,
+                "flat_damage": 7,
+                "penetration": 7,
+                "damage_type": "E",
+                "qualities": {
+                    "balanced": True,
+                    "power field": True
+                },
+                "class": "Melee",
+                "name": "Crozius Arcanum"
+            },
+            "astartes_power_fist": {
+                "dice": 2,
+                "flat_damage": 0,
+                "penetration": 9,
+                "damage_type": "E",
+                "qualities": {
+                    "power field": True,
+                    "unwieldy": True
+                },
+                "class": "Melee",
+                "name": "Astartes Power Fist"
+            },
+            "astartes_force_sword": {
+                "dice": 1,
+                "flat_damage": 2,
+                "penetration": 2,
+                "damage_type": "R",
+                "qualities": {
+                    "balanced": True,
+                    "special": True
+                },
+                "class": "Melee",
+                "name": "Astartes Force Sword"
+            },
+            "astartes_power_falchion": {
+                "dice": 1,
+                "flat_damage": 4,
+                "penetration": 6,
+                "damage_type": "E",
+                "qualities": {
+                    "razor sharp": True,
+                    "power field": True
+                },
+                "class": "Melee",
+                "name": "Astartes Power Falchion"
+            },
+            "bulkhead_shears": {
+                "dice": 2,
+                "flat_damage": 13,
+                "penetration": 6,
+                "damage_type": "R",
+                "qualities": {
+                    "tearing": True,
+                    "unwieldy": True
+                },
+                "class": "Melee",
+                "name": "Bulkhead Shears"
+            },
+            "astartes_force_staff": {
+                "dice": 1,
+                "flat_damage": 1,
+                "penetration": 0,
+                "damage_type": "I",
+                "qualities": {
+                    "balanced": True,
+                    "special": True
+                },
+                "class": "Melee",
+                "name": "Astartes Force Staff"
+            },
+            "astartes_thunder_hammer": {
+                "dice": 2,
+                "flat_damage": 5,
+                "penetration": 9,
+                "damage_type": "E",
+                "qualities": {
+                    "power field": True,
+                    "concussive": True,
+                    "unwieldy": True
+                },
+                "class": "Melee",
+                "name": "Astartes Thunder Hammer"
+            },
+            "astartes_power_claymore": {
+                "dice": 1,
+                "flat_damage": 11,
+                "penetration": 8,
+                "damage_type": "E",
+                "qualities": {
+                    "devastating": 1,
+                    "power field": True,
+                    "unwieldy": True
+                },
+                "class": "Melee",
+                "name": "Astartes Power Claymore"
+            },
+            "astartes_combat_knife": {
+                "dice": 1,
+                "flat_damage": 0,
+                "penetration": 0,
+                "damage_type": "R",
+                "qualities": {},
+                "class": "Melee",
+                "name": "Astartes Combat Knife"
+            },
+            "sacris_claymore": {
+                "dice": 2,
+                "flat_damage": 2,
+                "penetration": 2,
+                "damage_type": "R",
+                "qualities": {
+                    "unbalanced": True
+                },
+                "class": "Melee",
+                "name": "Sacris Claymore"
+            },
+            "rending_claws": {
+                "dice": 1,
+                "flat_damage": 0,
+                "penetration": 5,
+                "damage_type": "R",
+                "qualities": {
+                    "razor sharp": True
+                },
+                "class": "Melee",
+                "name": "Rending Claws"
+            },
+            "astartes_chainfist": {
+                "dice": 2,
+                "flat_damage": 0,
+                "penetration": 10,
+                "damage_type": "E",
+                "qualities": {
+                    "tearing": True,
+                    "power field": True,
+                    "unwieldy": True
+                },
+                "class": "Mounted",
+                "name": "Astartes Chainfist"
+            }
+        }
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
