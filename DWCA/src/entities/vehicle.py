27a2bf319facdef567c8f09bc7a660bde8703a0d
aran <Andreas.Ran@infor.com>
2018-02-24 15:11:59 +0100

    Added movement options for vehicles.

--- DWCA/src/entities/vehicle.py
+++ DWCA/src/entities/vehicle.py
@@ -1,4 +1,5 @@
 from src.dwca_log.log import get_log
+from src.entities import HALF_MOVE, FULL_MOVE, CHARGE_MOVE, RUN_MOVE
 from src.entities.character import Character
 
 
@@ -12,3 +13,21 @@ class Vehicle(Character):
 
     def get_modded_toughness_bonus(self, attack, hit_location):
         return 0
+
+    @property
+    def movement(self):
+        move_mod = self.tactical_speed
+        half_move = move_mod
+        full_move = move_mod * 2
+        charge_move = move_mod * 2
+        run_move = move_mod * 2
+        movement = {HALF_MOVE: half_move,
+                    FULL_MOVE: full_move,
+                    CHARGE_MOVE: charge_move,
+                    RUN_MOVE: run_move}
+        return movement
+
+    @property
+    def tactical_speed(self):
+        speed = self.get_stat('speed', 0)
+        return speed
